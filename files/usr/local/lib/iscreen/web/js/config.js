0b78e11be4a222f16b65a795907d7e31195f434f
Bernhard Bablok <bablokb@gmx.de>
2018-10-24 07:22:48 +0200

    added another URL

--- files/usr/local/lib/iscreen/web/js/config.js
+++ files/usr/local/lib/iscreen/web/js/config.js
@@ -11,13 +11,19 @@
 var UPDATE_INTERVAL = 10;     // in minutes
 
 var PAGES = [
-  {"url": "http://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?ld=96164&country=DEU&rt=1&input=F%FCrstenfeldbruck%238002141&boardType=dep&time=actual&productsFilter=11111&start=yes",
+  {"url":
+     "http://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?ld=96164&country=DEU&rt=1&input=F%FCrstenfeldbruck%238002141&boardType=dep&time=actual&productsFilter=11111&start=yes",
    "color": "red",
    "text": "Bahn"},
 
   {"url": "https://www.wetteronline.de/muenchen",
    "color": "green",
-   "text": "Wetter"},
+   "text": "Wetter M"},
+
+  {"url": 
+     "http://www.br.de/wettervorhersage/wetterprognose/82256/F%C3%BCrstenfeldbruck",
+   "color": "pink",
+   "text": "Wetter F"},
 
   {"url": "https://raspifeed.com",
    "color": "yellow",

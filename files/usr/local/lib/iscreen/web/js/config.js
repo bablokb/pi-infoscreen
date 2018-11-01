7a7ae7a5c4dbea3da9932d5ffffc3c0280a2a081
Bernhard Bablok <bablokb@gmx.de>
2018-11-01 19:46:11 +0100

    added reload/cycle-variables

--- files/usr/local/lib/iscreen/web/js/config.js
+++ files/usr/local/lib/iscreen/web/js/config.js
@@ -8,28 +8,36 @@
 //
 // ----------------------------------------------------------------------------
 
-var UPDATE_INTERVAL = 20;     // in seconds
-
 var PAGES = [
   {"url":
      "http://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?ld=96164&country=DEU&rt=1&input=F%FCrstenfeldbruck%238002141&boardType=dep&time=actual&productsFilter=11111&start=yes",
    "color": "red",
-   "text": "Bahn"},
+   "text": "Bahn",
+   "reload": 60,
+   "cycle": 20 },
 
   {"url": "https://www.wetteronline.de/muenchen",
    "color": "green",
-   "text": "Wetter M"},
+   "text": "Wetter M",
+   "reload": 300,
+   "cycle": 15 },
 
   {"url": 
      "http://www.br.de/wettervorhersage/wetterprognose/82256/F%C3%BCrstenfeldbruck",
    "color": "pink",
-   "text": "Wetter F"},
+   "text": "Wetter F",
+   "reload": 300,
+   "cycle": 20 },
 
   {"url": "https://raspifeed.com",
    "color": "yellow",
-   "text": "Raspifeed"},
+   "text": "Raspifeed",
+   "reload": 600,
+   "cycle": 10 },
 
   {"url": "https://sueddeutsche.de/news",
    "color": "blue",
-   "text": "SZ"}
+   "text": "SZ",
+   "reload": 60,
+   "cycle": 20 }
 ];
